4e59c7d055816248b55654ea9ed2b3165d18c8c8
Zhanliang Liu <zhanliangliu@qq.com>
2018-08-17 09:11:12 +0800

    update local_grammar_test.go

--- local_grammar_test.go
+++ local_grammar_test.go
@@ -11,6 +11,7 @@ func TestLocalGrammar(t *testing.T) {
 	tests := []string{
 		`天津，liang@zliu.org是我的邮箱，https://crawler.club是爬虫主页`,
 		`关于FMR的介绍在这里：https://zliu.org/project/fmr/,好的`,
+		`柏乡县是一个历史悠久的小城，高邑县也是，南开区呢，海淀区，思明区在哪里`,
 	}
 	for _, c := range tests {
 		d := ling.NewDocument(c)
