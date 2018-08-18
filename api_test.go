1fa519cb373770b39ff44425daa5f4cc8b29d83e
liuzhanliang <zhanliangliu@qq.com>
2018-08-18 21:06:40 +0800

    add api_test.go

---
+++ api_test.go
@@ -0,0 +1,63 @@
+package fmr
+
+import (
+	"testing"
+)
+
+func TestLocalParse(t *testing.T) {
+	tests := []string{
+		`柏乡位于河北省`,
+	}
+	g := &Grammar{}
+	for _, c := range tests {
+		ps, err := g.EarleyParseMaxAll(c, "loc_province", "loc_county")
+		if err != nil {
+			t.Error(err)
+		}
+		for _, p := range ps {
+			for _, f := range p.GetFinalStates() {
+				t.Log(f)
+				trees := p.GetTrees(f)
+				t.Log(trees)
+				for _, tree := range trees {
+					sem, err := tree.Semantic()
+					if err != nil {
+						t.Error(err)
+					}
+					t.Log(sem)
+				}
+			}
+		}
+	}
+}
+
+func TestGParse(t *testing.T) {
+	tests := []string{
+		`柏乡位于河北省`,
+	}
+	grammar := `<loc> = <loc_province> {nf.loc($1)}| <loc_county> {nf.loc($1)};`
+	g, err := CFGrammar(grammar)
+	if err != nil {
+		t.Error(err)
+	}
+	for _, c := range tests {
+		ps, err := g.EarleyParseMaxAll(c, "loc")
+		if err != nil {
+			t.Error(err)
+		}
+		for _, p := range ps {
+			for _, f := range p.GetFinalStates() {
+				t.Log(f)
+				trees := p.GetTrees(f)
+				t.Log(trees)
+				for _, tree := range trees {
+					sem, err := tree.Semantic()
+					if err != nil {
+						t.Error(err)
+					}
+					t.Log(sem)
+				}
+			}
+		}
+	}
+}
