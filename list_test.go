b36e4f1b9640cadc10f42e99296cbf3aa16239de
liuzhanliang <zhanliangliu@qq.com>
2018-12-28 22:27:18 +0800

    add regex grammar test case

--- list_test.go
+++ list_test.go
@@ -10,6 +10,8 @@ func TestList(t *testing.T) {
 	cases := []string{
 		`直辖市：北京上海天津`,
 		`直辖市：北京、上海和天津`,
+		`直辖市：北京、上海和天津、津城`,
+		`直辖市：帝都、魔都、寨都、旧都`,
 	}
 	g, err := GrammarFromFile("sf.grammar")
 	if err != nil {
