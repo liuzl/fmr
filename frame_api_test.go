7144fb46615f7884b1a84f12831bb7b11aedeb89
liuzhanliang <zhanliangliu@qq.com>
2020-05-09 11:29:56 +0800

    add TestMatchFrames2

--- frame_api_test.go
+++ frame_api_test.go
@@ -22,3 +22,21 @@ func TestMatchFrames(t *testing.T) {
 		t.Log(c, fmrs)
 	}
 }
+
+func TestMatchFrames2(t *testing.T) {
+	cases := []string{
+		`获得亚军次数降序排前5的都是哪些羽毛球运动员？`,
+		`注册资本大于1亿的品牌中，哪5个品牌收入最少？并给出它们的法定代表人`,
+	}
+	g, err := GrammarFromFile("grammars/sql.grammar")
+	if err != nil {
+		t.Error(err)
+	}
+	for _, c := range cases {
+		fmrs, err := g.FrameFMR(c)
+		if err != nil {
+			t.Error(err)
+		}
+		t.Log(c, fmrs)
+	}
+}
